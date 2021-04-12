74bdb582681f1d65874d1b8ba5512734ef6d18a4
Jesus Lara <jelara99@gmail.com>
2021-04-12 20:54:52 +0000

    ejercicio resuelto

--- exercises/12.5-Yes_and_no/app.py
+++ exercises/12.5-Yes_and_no/app.py
@@ -2,4 +2,11 @@ theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
 
 #Your code go here:
 
+def selector(x):
+    if x ==1:
+        return 'wiki'
+    else:
+        return 'woko'
 
+salida=list(map(selector,theBools))
+print(salida)
